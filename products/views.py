919ba46011400de40a1d95943e7c635c1e069159
tealhoion87 <nealtoms@gmail.com>
2021-12-01 12:36:36 +0000

    Install products to products page
      - add to the all_products view to send all products tot he front end
        for rendering
      - iterate through the products, creating a card for each on the
        products page
      - change how the footer works so that it is only sticky on the home
        page using JavaScript

--- products/views.py
+++ products/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 def all_products(request):
     """ A view to return all products page """
 
-    return render(request, "products/products.html")
+    products = Product.objects.all()
+
+    context = {
+        "products": products,
+    }
+
+    return render(request, "products/products.html", context)
