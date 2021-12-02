68ff29b4936b1aaace3507b84124e1cbd145f6af
tealhoion87 <nealtoms@gmail.com>
2021-12-02 12:17:49 +0000

    Add product detail template and link to other pages

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 def all_products(request):
@@ -10,4 +10,15 @@ def all_products(request):
         "products": products,
     }
 
-    return render(request, "products/products.html", context)
+    return render(request, "products/products_main.html", context)
+
+
+def product_detail(request, product_id):
+    """ A view to render the product details page """
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        "product": product
+    }
+
+    return render(request, "products/product_detail.html", context)
