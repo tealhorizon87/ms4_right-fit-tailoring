e286cb5cf4ce0b79b016027a4fc5b01f0a449cfa
tealhoion87 <nealtoms@gmail.com>
2021-12-08 12:28:41 +0000

    Add the 'add_to_cart' view to the cart app
      - add a form to the product detail template to handle quantity and
        sending the item to the cart
      - add a contexts file to handle the cart contents and make it
        available site-wide
      - add a new style class for a reverse-coloured button for better
        contrast

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
+    """ A view to display the cart contents """
+
     return render(request, "cart/cart.html")
+
+
+def add_to_cart(request, item_id):
+    """ A view to add a quantity of the same item to the cart """
+
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+    cart = request.session.get("cart", {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session["cart"] = cart
+    print(request.session["cart"])
+
+    return redirect(redirect_url)
