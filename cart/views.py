75b4c8fecba00650bd6551616cf783735afced92
tealhoion87 <nealtoms@gmail.com>
2021-12-16 15:36:13 +0000

    Add update and remove functions to the cart
      - add a view to update the quantity of the item in the cart
      - add a view to remove the item from the shopping cart
      - make initial styling; to be improved later

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def view_cart(request):
@@ -32,5 +34,48 @@ def add_to_cart(request, item_id):
             cart[item_id] = quantity
 
     request.session["cart"] = cart
-
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+    """ A view to update the quantity of the same item to the cart """
+
+    quantity = int(request.POST.get("quantity"))
+    size = None
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+    cart = request.session.get("cart", {})
+
+    if size:
+        if quantity > 0:
+            cart[item_id]["items_by_size"][size] = quantity
+            messages.success(request, "Item updated")
+        else:
+            del cart[item_id]["items_by_size"][size]
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+            messages.success(request, "Item updated")
+        else:
+            cart.pop(item_id)
+
+    request.session["cart"] = cart
+    return redirect(reverse("cart"))
+
+
+def remove_from_cart(request, item_id):
+    """ A view to remove the entire item from the cart """
+
+    product = get_object_or_404(Product, pk=item_id)
+    size = None
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+    cart = request.session.get("cart", {})
+
+    if size:
+        del cart[item_id]["items_by_size"][size]
+    else:
+        cart.pop(item_id)
+
+    request.session["cart"] = cart
+    return redirect(reverse("cart"))
