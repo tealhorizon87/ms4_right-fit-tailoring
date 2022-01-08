ebe6c6e03f5f1282eaa53bf981e10aa1b1008277
tealhoion87 <nealtoms@gmail.com>
2022-01-08 12:53:39 +0000

    Add cutom styling to allauth and draw profile to template

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from checkout.models import Order
+from .models import UserProfile
 
 
 def profile(request):
-    return render(request, "profiles/profile.html")
+    """ Display the user's profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    context = {
+        "profile": profile,
+    }
+    return render(request, "profiles/profile.html", context)
