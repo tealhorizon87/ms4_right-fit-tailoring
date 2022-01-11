0d527ee8899ceefd2d297e0d14abaa15101079a2
tealhoion87 <nealtoms@gmail.com>
2022-01-11 12:31:59 +0000

    Attempt3-fix heroku deployment

--- right_fit_tailoring/settings.py
+++ right_fit_tailoring/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1", "tealhorizon87-rf-tailoring.herokuapp.com"]
+ALLOWED_HOSTS = ["tealhorizon87-rf-tailoring.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
--- right_fit_tailoring/wsgi.py
+++ right_fit_tailoring/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'right_fit_tailoring.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'right_fit_tailoring.settings')
 
 application = get_wsgi_application()
