039a449098c8aaab4182e4b266904465f524b450
tealhoion87 <nealtoms@gmail.com>
2022-01-13 11:25:46 +0000

    Revert checkout app and start rebuild for webhooks

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,14 +6,13 @@
 */
 
 var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
-var clientSecret = $("#id_client_secret").text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 
 var style = {
   base: {
     color: '#000',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontFamily: '"Josefin Sans", sans-serif',
     fontSmoothing: 'antialiased',
     fontSize: '16px',
     '::placeholder': {
@@ -28,91 +27,3 @@ var style = {
 
 var card = elements.create("card", {style, style});
 card.mount("#card-element");
-
-// Handle realtime validation errors on the card element
-card.addEventListener('change', function (event) {
-  var errorDiv = document.getElementById('card-errors');
-  if (event.error) {
-    var html = `
-      <span class="icon" role="alert">
-        <i class="fas fa-times"></i>
-      </span>
-      <span>${event.error.message}</span>
-    `;
-    $(errorDiv).html(html);
-  } else {
-    errorDiv.textContent = '';
-  }
-});
-
-// Handle form submit
-var form = document.getElementById('payment-form');
-
-form.addEventListener('submit', function(ev) {
-  ev.preventDefault();
-  card.update({ 'disabled': true});
-  $('#submit-button').attr('disabled', true);
-  $("#payment-form").fadeToggle(100);
-  $("#loading-overlay").fadeToggle(100);
-
-  var saveInfo = Boolean($("#id-save-info").attr("checked"));
-  var csrfToken = $("input[name='csrfmiddlewaretoken']").val();
-  var postData = {
-    "csrfmiddlewaretoken": csrfToken,
-    "client_secret": clientSecret,
-    "save_info": saveInfo,
-  };
-  var url = "/checkout/cache_checkout_data/";
-
-  $.post(url, postData).done(function() {
-    stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: card,
-        billing_details: {
-          name: $.trim(form.full_name.vlaue),
-          phone: $.trim(form.phone_number.vlaue),
-          email: $.trim(form.email.vlaue),
-          address: {
-            line1: $.trim(form.address_line_1.vlaue),
-            line2: $.trim(form.address_line_2.vlaue),
-            city: $.trim(form.city.vlaue),
-            county: $.trim(form.county.vlaue),
-            country: $.trim(form.country.vlaue),
-          }
-        }
-      },
-      shipping: {
-        name: $.trim(form.full_name.vlaue),
-        phone: $.trim(form.phone_number.vlaue),
-        address: {
-          line1: $.trim(form.address_line_1.vlaue),
-          line2: $.trim(form.address_line_2.vlaue),
-          city: $.trim(form.city.vlaue),
-          county: $.trim(form.county.vlaue),
-          postcode: $.trim(form.postcode.value),
-          country: $.trim(form.country.vlaue),
-        }
-      }
-    }).then(function(result) {
-      if (result.error) {
-        var errorDiv = document.getElementById('card-errors');
-        var html = `
-            <span class="icon" role="alert">
-            <i class="fas fa-times"></i>
-            </span>
-            <span>${result.error.message}</span>`;
-        $(errorDiv).html(html);
-        $("#payment-form").fadeToggle(100);
-        $("#loading-overlay").fadeToggle(100);
-        card.update({ 'disabled': false});
-        $('#submit-button').attr('disabled', false);
-      } else {
-        if (result.paymentIntent.status === 'succeeded') {
-          form.submit();
-        }
-      }
-    });
-  }).fail(function() {
-    location.reload();
-  });
-});
--- checkout/views.py
+++ checkout/views.py
@@ -1,174 +1,23 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
-from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from django.conf import settings
 from .forms import OrderForm
-from .models import Order, OrderLineItem
-from products.models import Product
-from profiles.forms import UserProfileForm
-from profiles.models import UserProfile
-from cart.contexts import cart_contents
-import stripe
-import json
-
-
-@require_POST
-def cache_checkout_data(request):
-    try:
-        pid = request.POST.get("client_secret").split("_secret")[0]
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        stripe.PaymentIntent.modify(pid, metadata={
-            "cart": json.dumps(request.session.get("cart", {})),
-            "save_info": request.POST.get("save_info"),
-            "username": request.user,
-        })
-        return HttpResponse(status=200)
-    except Exception as e:
-        messages.error(request, "Sorry, your payment cannot be \
-            processed right now. Please try again later.")
-        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == 'POST':
-        cart = request.session.get('cart', {})
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your cart at the moment")
+        return redirect(reverse('products'))
 
-        form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'address_line_1': request.POST['address_line_1'],
-            'address_line_2': request.POST['address_line_2'],
-            'city': request.POST['city'],
-            'county': request.POST['county'],
-            'postcode': request.POST['postcode'],
-            'country': request.POST['country'],
-        }
-        order_form = OrderForm(form_data)
-        if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get("client_secret").split("_secret")[0]
-            order.stripe_pid = pid
-            order.original_cart = json.dumps(cart)
-            order.save()
-
-            for item_id, item_data in cart.items():
-                try:
-                    product = Product.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
-                        )
-                        order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                                size=size,
-                            )
-                            order_line_item.save()
-                except Product.DoesNotExist:
-                    messages.error(request, (
-                        "One of the products in your cart wasn't found in our database. "
-                        "Please call us for assistance!")
-                    )
-                    order.delete()
-                    return redirect(reverse('view_cart'))
-
-            request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
-        else:
-            messages.error(request, 'There was an error with your form. \
-                Please double check your information.')
-    else:
-        cart = request.session.get('cart', {})
-        if not cart:
-            messages.error(request, "There's nothing in your cart at the moment")
-            return redirect(reverse('products'))
-
-        current_cart = cart_contents(request)
-        total = current_cart["total"]
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
-
-        if request.user.is_authenticated:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(initial={
-                    'full_name': profile.user.get_full_name(),
-                    'email': profile.default_email,
-                    'phone_number': profile.default_phone_number,
-                    'address_line_1': profile.default_address_line_1,
-                    'address_line_2': profile.default_address_line_2,
-                    'city': profile.default_city,
-                    'county': profile.default_county,
-                    'postcode': profile.default_postcode,
-                    'country': profile.default_country,
-                })
-            except UserProfile.DoesNotExist:
-                order_form = OrderForm()
-        else:
-            order_form = OrderForm()
-
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing. \
-                Did you forget to set it in your environment?')
-
-        context = {
-            "form": order_form,
-            "stripe_public_key": stripe_public_key,
-            'client_secret': intent.client_secret,
-        }
-
-        return render(request, "checkout/checkout.html", context)
-
-
-def checkout_success(request, order_number):
-    """
-    Handle successful checkouts
-    """
-    save_info = request.session.get("save_info")
-    order = get_object_or_404(Order, order_number=order_number)
-
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
-        order.user_profile = profile
-        order.save()
-
-        if save_info:
-            profile_data = {
-                "default_phone_number": order.phone_number,
-                "default_address_line_1": order.address_line_1,
-                "default_address_line_2": order.address_line_2,
-                "default_city": order.city,
-                "default_county": order.county,
-                "default_postcode": order.postcode,
-                "default_country": order.country,
-            }
-            user_profile_form = UserProfileForm(profile_data, instance=profile)
-            if user_profile_form.is_valid():
-                user_profile_form.save()
-
-    messages.success(request, f"Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.")
-
-    if "bag" in request.session:
-        del request.session["cart"]
+    order_form = OrderForm()
 
     context = {
-        "order": order,
+        "form": order_form,
+        "stripe_public_key": stripe_public_key,
+        'client_secret': "test client secret",
     }
 
-    return render(request, "checkout/checkout_success.html", context)
+    return render(request, "checkout/checkout.html", context)
