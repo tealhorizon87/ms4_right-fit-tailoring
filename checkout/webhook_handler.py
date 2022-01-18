1cee33eae727b99c4b4d9f1b3f0b25a0d5861ee6
tealhoion87 <nealtoms@gmail.com>
2022-01-18 11:24:18 +0000

    Add imports to webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from .models import Order, OrderLineItem
+from products.models import Product
+from profiles.models import UserProfile
+import json
+import time
 
 
 class StripeWH_Handler:
